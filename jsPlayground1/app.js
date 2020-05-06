c96a98ceec1d8f6bea4714c8d6bb85ffea0ad272
MatejaTrik <mtrikic2@gmail.com>
2020-05-06 21:12:34 +0200

    06.05.2020.

---
+++ jsPlayground1/app.js
@@ -0,0 +1,22 @@
+//log to conslole
+console.log('Hello World')
+console.log(123)
+console.log(true)
+var geerting = "Hello everyone"
+console.log(geerting)
+console.log([1,2,3,4])
+console.log({a:1, b:2})
+console.table({a:1, b:2})
+
+console.error('this is some error')
+
+console.clear()
+console.warn('this is a warning')
+console.time("Hello World: timer = ")
+    console.log('Hello World')
+    console.log('Hello World')
+    console.log('Hello World')
+    console.log('Hello World')
+    console.log('Hello World')
+    console.log('Hello World')
+console.timeEnd("Hello World: timer = ")
---
+++ jsPlayground2/app.js
@@ -0,0 +1,21 @@
+// var, let, const
+
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// init var
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting)
+
+//letters, numbers, _, $
+//can not star with number
+
+//multivord vars
+var firstName = 'John'; //camelCase
+var frist_name = 'Sara'; //under_score
+var FirstName = 'Tom'; //PascalCase
+
