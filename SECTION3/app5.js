10c8ec171b51cf97b0f7b5b3bd1c01bf1f112f27
MatejaTrik <mtrikic2@gmail.com>
2020-05-14 13:52:26 +0200

    14.05.2020.

--- SECTION3/app5.js
+++ SECTION3/app5.js
@@ -1,17 +1,38 @@
 const form = document.querySelector('form');
 const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5')
 
 //clear input
 taskInput.value = '';
 
 //form.addEventListener('submit', runEvent)
 
+//key down
 taskInput.addEventListener('keydown', runEvent)
+
+//key up
+taskInput.addEventListener('keyup', runEvent)
+
+//focus 
+taskInput.addEventListener('focus', runEvent)
+
+//blur
+taskInput.addEventListener('blur', runEvent)
+
+//cut 
+taskInput.addEventListener('cut', runEvent)
+
+//paste
+taskInput.addEventListener('cut', runEvent)
+
+
 
 function runEvent (e) {
     console.log(`EVENT TYPE: ${e.type}`)
 
     console.log(taskInput.value)
+
+    heading.innerText = e.target.value
     
     //Get input value
     //console.log(taskInput.value)
